df3ed34078817bfc2736416493a687ef8595d76a
akez <1791189210@qq.com>
2021-09-02 15:29:00 +0800

    alert

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -18,8 +18,7 @@ const Index = () => {
   const [selector] = useState(['美国', '中国', '巴西', '日本']);
   const [selected, setSelected] = useState('美国');
 
-  const handlePickerOnChange = (e)=>{
-    alert(e.detail.value)
+  const handlePickerOnChange = (e) => {
     setSelected(selector[e.detail.value]);
   };
 
@@ -29,11 +28,11 @@ const Index = () => {
       <Editor id='editor' placeholder='please input' />
       <Button size='mini' type='default'>Button</Button>
       <Picker mode='selector' range={selector} onChange={handlePickerOnChange}>
-          <View>
-            <Text>
-              Selected: {selected}
-            </Text>
-          </View>
+        <View>
+          <Text>
+            Selected: {selected}
+          </Text>
+        </View>
       </Picker>
     </View>
   );
