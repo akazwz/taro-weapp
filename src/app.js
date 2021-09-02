086d9e7f1c7c6c1a30d02929009f39068ea12ba3
akez <1791189210@qq.com>
2021-09-02 14:33:35 +0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from 'react';
+import {useDidShow, useDidHide} from '@tarojs/taro';
+import {Provider} from 'react-redux';
+import store from './redux/store';
+import './app.scss';
+
+const App = (props) => {
+  useEffect(() => {
+  });
+
+  useDidShow(() => {
+  });
+
+  useDidHide(() => {
+  });
+
+  return (
+    <Provider store={store}>
+      {props.children}
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/pages/index/index.jsx
@@ -0,0 +1,42 @@
+import {useEffect, useState} from 'react';
+import {View, Icon, Editor, Button, Picker, Text} from '@tarojs/components';
+import {useReady, useDidShow, useDidHide, usePullDownRefresh,} from '@tarojs/taro';
+import './index.scss';
+
+const Index = () => {
+  useEffect(() => {
+  });
+  useReady(() => {
+  });
+  useDidShow(() => {
+  });
+  useDidHide(() => {
+  });
+  usePullDownRefresh(() => {
+  });
+
+  const [selector] = useState(['美国', '中国', '巴西', '日本']);
+  const [selected, setSelected] = useState('美国');
+
+  const handlePickerOnChange = (e)=>{
+    alert(e.detail.value)
+    setSelected(selector[e.detail.value]);
+  };
+
+  return (
+    <View className='index'>
+      <Icon size='60' type='success' />
+      <Editor id='editor' placeholder='please input' />
+      <Button size='mini' type='default'>Button</Button>
+      <Picker mode='selector' range={selector} onChange={handlePickerOnChange}>
+          <View>
+            <Text>
+              Selected: {selected}
+            </Text>
+          </View>
+      </Picker>
+    </View>
+  );
+};
+
+export default Index;
